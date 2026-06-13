5eb231ace3f8d846a8ba2241f4fc374bd567a614
yzzy0000 <rayojo67@gmail.com>
2026-06-13 11:18:58 -0400

    Remove logging of chat messages
    Removed console log for incoming chat messages.

--- chat-server/server.js
+++ chat-server/server.js
@@ -7,20 +7,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve files from the public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Socket.IO chat
 io.on("connection", (socket) => {
   console.log("User connected");
 
   socket.on("chat", (message) => {
-    console.log("Message:", message);
     io.emit("chat", message);
   });
 
